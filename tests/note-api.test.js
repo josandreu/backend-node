cc78a60a81c3f7c4aa317f74fc5e3b794c2bed5e
joseantonio_andreu <joseantonio.andreu@flat101.es>
2024-09-27 12:51:03 +0200

    add users

--- tests/note-api.test.js
+++ tests/note-api.test.js
@@ -2,12 +2,14 @@ const { test, after, beforeEach, describe } = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const bcrypt = require('bcrypt');
 const app = require('../app');
 const api = supertest(app);
 
 const helper = require('./test-helper');
 
 const Note = require('../models/note');
+const User = require('../models/user');
 
 const initialNotes = helper.initialNotes;
 
@@ -164,6 +166,61 @@ describe('when there is initially some notes saved', () => {
       const contents = notesAtEnd.map((r) => r.content);
       assert(!contents.includes(noteToDelete.content));
     });
+  });
+});
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10);
+    const user = new User({ username: 'root', passwordHash });
+
+    await user.save();
+  });
+
+  test('should creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    };
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((user) => user.username);
+    assert(usernames.includes(newUser.username));
+  });
+
+  test('should creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    };
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+
+    assert(result.body.error.includes('expected `username` to be unique'));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
   });
 });
 
